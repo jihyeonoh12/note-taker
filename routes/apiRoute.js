c0244b9c1c60dc72e870c1b14b5b3d2029e7523c
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-04-25 22:58:43 -0700

    final save

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,13 +1,43 @@
 var storedData = require("../db/db.json");
-console.log(storedData);
+
 
 module.exports = function(app) {
     
-    app.get("/api/notes"), function(req, res) {
-        res.json(storedData);
-        
-    }
+    app.get("/api/notes", function(req, res) {
+       return res.json(storedData);
+       
+    });
 
+    app.post("/api/notes", function(req, res) {
+       
+        
+        console.log(newNote);
+        console.log("----");
+        console.log(req.body);
+        var newNote = req.body;
+        storedData.push(newNote);
+        res.json(storedData); 
+
+      });
+
+    app.delete("/api/notes", function(req, res) {
+      console.log("get delete here");
+       var listItemName = req.body;
+       console.log(listItemName);
+       delete storedData[{listItemName}];
+       console.log(storedData);
+      
+    //    var filtered = storedData.filter(function (listItemName) {
+    //      item.noteTitle !== listItemName;
+    //     storedData.push(filtered);
+      
+         return res.json(storedData);
+
+    // });
+
+   
+
+    });
 
 };
 
